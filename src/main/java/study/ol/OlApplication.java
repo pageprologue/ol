454ddc85514f6e6ad33c7ec561a50de8e45736c0
Chloe <page.prologue@gmail.com>
2020-08-19 17:02:56 +0900

    Initial Project Setting

---
+++ src/main/java/study/ol/OlApplication.java
@@ -0,0 +1,13 @@
+package study.ol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/study/ol/OlApplicationTests.java
@@ -0,0 +1,13 @@
+package study.ol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
